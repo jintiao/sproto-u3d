1120c68cc2bb7953849b80019a6d7bfcd833c9cc
jintiao <jintiao@outlook.com>
2014-09-23 20:44:12 +0800

    add test

---
+++ Assets/test/Test.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Test {
+	public void Run () {
+		LoadProto ();
+		
+		SpObject obj = CreateObject ();
+		Debug.Log (Util.DumpObject (obj));
+		
+		MemoryStream encode_stream = new MemoryStream ();
+		SpCodec.Encode ("AddressBook", obj, encode_stream);
+		
+		encode_stream.Position = 0;
+		Debug.Log (Util.DumpStream (encode_stream));
+		
+		encode_stream.Position = 0;
+		MemoryStream pack_stream = new MemoryStream ();
+		SpPacker.Pack (encode_stream, pack_stream);
+		
+		pack_stream.Position = 0;
+		Debug.Log (Util.DumpStream (pack_stream));
+		
+		pack_stream.Position = 0;
+		MemoryStream unpack_stream = new MemoryStream ();
+		SpPacker.Unpack (pack_stream, unpack_stream);
+		
+		unpack_stream.Position = 0;
+		Debug.Log (Util.DumpStream (unpack_stream));
+		
+		unpack_stream.Position = 0;
+		SpObject newObj = SpCodec.Decode ("AddressBook", unpack_stream);
+		Debug.Log (Util.DumpObject (newObj));
+	}
+	
+	private SpObject CreateObject () {
+		SpObject obj = new SpObject ();
+
+		SpObject person = new SpObject ();
+
+		{
+			SpObject p = new SpObject ();
+			p.Insert ("name", "Alice");
+			p.Insert ("id", 10000);
+
+			SpObject phone = new SpObject ();
+			{
+				SpObject p1 = new SpObject ();
+				p1.Insert ("number", "123456789");
+				p1.Insert ("type", 1);
+				phone.Append (p1);
+			}
+			{
+				SpObject p1 = new SpObject ();
+				p1.Insert ("number", "87654321");
+				p1.Insert ("type", 2);
+				phone.Append (p1);
+			}
+			p.Insert ("phone", phone);
+
+			person.Append (p);
+		}
+		{
+			SpObject p = new SpObject ();
+			p.Insert ("name", "Bob");
+			p.Insert ("id", 20000);
+			
+			SpObject phone = new SpObject ();
+			{
+				SpObject p1 = new SpObject ();
+				p1.Insert ("number", "01234567890");
+				p1.Insert ("type", 1);
+				phone.Append (p1);
+			}
+			p.Insert ("phone", phone);
+			
+			person.Append (p);
+		}
+		
+		return obj;
+	}
+	
+	public void LoadProto () {
+		string path = Application.dataPath + "/AddressBook.sproto";
+		
+		using (FileStream stream = new FileStream (path, FileMode.Open)) {
+			SpTypeManager.Import (stream);
+		}
+	}
+}
---
+++ Assets/test/Util.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+public class Util {
+	public static string DumpStream (Stream stream) {
+		string str = "";
+		
+		byte[] buf = new byte[16];
+		int count;
+		
+		while ((count = stream.Read (buf, 0, buf.Length)) > 0) {
+			str += DumpLine (buf, count);
+		}
+		
+		return str;
+	}
+	
+	private static string DumpLine (byte[] buf, int count) {
+		string str = "";
+		
+		for (int i = 0; i < count; i++) {
+			str += ((i < count) ? String.Format ("{0:X2}", buf[i]) : "  ");
+			str += ((i > 0) && (i < count - 1) && ((i + 1) % 8 == 0) ? " - " : " ");
+		}
+		str += "\n";
+		
+		return str;
+	}
+	
+	public static string DumpObject (SpObject obj) {
+		return DumpObject (obj, 0);
+	}
+	
+	private static string DumpObject (SpObject obj, int tab) {
+		string str = "";
+		
+		if (obj.IsTable ()) {
+			str = GetTab (tab) + "<table>\n";
+			foreach (KeyValuePair<string, SpObject> entry in obj.ToTable ()) {
+				str += GetTab (tab + 1) + "<key : " + entry.Key + ">\n";
+				str += DumpObject (entry.Value, tab + 1);
+			}
+		}
+		else if (obj.IsArray ()) {
+			str = GetTab (tab) + "<array>\n";
+			foreach (SpObject o in obj.ToArray ()) {
+				str += DumpObject (o, tab + 1);
+			}
+		}
+		else if (obj.IsBuildinType ()) {
+			str = GetTab (tab) + obj.Value.ToString () + "\n";
+		}
+		
+		return str;
+	}
+	
+	private static string GetTab(int n) {
+		string str = "";
+		for (int i = 0; i < n; i++)
+			str += "\t";
+		return str;
+	}
+}
