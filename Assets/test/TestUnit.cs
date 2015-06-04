3b2790a3974c4d831004965376ab00b332faf7ce
jintiao <jintiao@outlook.com>
2015-06-05 00:36:14 +0800

    nest type test

--- Assets/test/TestUnit.cs
+++ Assets/test/TestUnit.cs
@@ -39,14 +39,107 @@ public class TestUnit {
 		Util.Assert (s == ds);
 	}
 
+	private void TestNest () {
+		SpObject clist = new SpObject (SpObject.ArgType.Table,
+				"character",
+				new SpObject (SpObject.ArgType.Array, 
+						new SpObject (SpObject.ArgType.Table,
+								"id", 
+		              			1,
+								"attribute", 
+		              			new SpObject (SpObject.ArgType.Table,
+		                 				"level", 
+		              					1,
+		                 				"templateId", 
+		              					1001,
+		                 				"ability", 
+		              					new SpObject (SpObject.ArgType.Table,
+		                 						"hp", 2530,
+		                 						"att", 2310
+		                 				)
+		                 		)
+						),
+						new SpObject (SpObject.ArgType.Table,
+								"id", 
+		              			2,
+		              			"attribute", 
+		              			new SpObject (SpObject.ArgType.Table,
+		                 				"level", 
+		              					1,
+		                 				"templateId", 
+		              					1002,
+		                 				"ability", 
+		              					new SpObject (SpObject.ArgType.Table,
+		                 						"hp", 1320,
+		                 						"att", 2090
+		                 				)
+		                 		)
+						)
+				)
+		);
+		
+		Util.Log ("------------------TEST CHARACTER----------------------------");
+		Util.DumpObject (clist);
+		
+		Util.Log ("Encode");
+		SpStream encode_stream = new SpStream ();
+		manager.Codec.Encode ("clist", clist, encode_stream);
+		encode_stream.Position = 0;
+		Util.DumpStream (encode_stream);
+		
+		Util.Log ("Pack");
+		encode_stream.Position = 0;
+		SpStream pack_stream = new SpStream ();
+		SpPacker.Pack (encode_stream, pack_stream);
+		pack_stream.Position = 0;
+		Util.DumpStream (pack_stream);
+		
+		Util.Log ("Unpack");
+		pack_stream.Position = 0;
+		SpStream unpack_stream = new SpStream ();
+		SpPacker.Unpack (pack_stream, unpack_stream);
+		unpack_stream.Position = 0;
+		Util.DumpStream (unpack_stream);
+		
+		Util.Log ("Decode");
+		unpack_stream.Position = 0;
+		SpObject dobj = manager.Codec.Decode ("clist", unpack_stream);
+		Util.DumpObject (dobj);
+	}
+
 	public void Run () {
 		
 		string proto = @"
 			.ss {
 				a 0 : string
 			}
+
+			.ability {
+				hp 0 : integer
+				att 1 : integer
+			}
+
+			.attribute {
+				templateId 0 : integer
+				level 1 : integer
+				ability 2 : ability
+			}
+
+			.character {
+				id 0 : integer
+				attribute 1 : attribute
+			}
+
+			.clist {
+				character 0 : *character(id)
+			}
+			
 		";
 		manager = SpTypeManager.Import (proto);
+
+
+
+		TestNest ();
 		
 		TestStr ("");
 		TestStr ("123");
@@ -57,7 +150,7 @@ public class TestUnit {
 		TestStr ("1234567812345678");
 		TestStr ("12345678123456781234567812345678");
 		TestStr ("123456781234567812345678123456781");
-		TestStr ("123456781234567812345678123456781234567");
+		TestStr ("123456781234567812345678123456781234567"); 
 	}
 	
 	private void CheckObj (SpObject obj) {
